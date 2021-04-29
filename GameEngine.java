bd4d31efe2b4bda67ec63cf00e69760a024fae2b
jdelles <jdelles@ncsu.edu>
2021-04-29 18:24:22 -0400

    [IMPLEMENTATION] Working game

--- GameEngine.java
+++ GameEngine.java
@@ -1,87 +1,96 @@
 /**
- * The Game Engine interfaces with the GUI and contains the game logic 
- * for the Tic Tac Toe game
+ * The Game Engine interfaces with the GUI and contains the game logic.
  */
 public class GameEngine {
 
-    private GameEngine instance; 
-
-    private char playerX = 'X'; 
-
-    private char playerO = 'O'; 
-
-    private char currentPlayer; 
+    /**
+     * String representation of the X player. Used to track where Xs have been played. 
+     */
+    private String playerX = "X"; 
 
     /**
-     * A standard 3x3 tic tac toe grid 
+     * String representation of the O player. Used to track where Os have been played. 
+     */
+    private String playerO = "O"; 
+
+    /**
+     * Tracks the current player. Used to update text in the GUI and to declare a winner. 
+     */
+    private String currentPlayer; 
+
+    /**
+     * A standard 3x3 tic tac toe grid constructed with a 2D String array
      * [0,0] [0,1] [0,2]
      * [1,0] [1,1] [1,2]
      * [2,0] [2,1] [2,2]
      */
-    private char[][] gameBoard; 
+    private String[][] gameBoard; 
     
     /**
-     * Game Engine Constructor
+     * Game Engine Constructor. 
+     * Sets the current player to X. 
+     * Initializes the gameBoard array and fills it with empty strings. 
      */
     public GameEngine() {
-        newGameEngine(); 
-    }
-
-    /**
-     * restricts the GameEngine to a singular instance
-     */
-    private void newGameEngine() {
-        if (null == instance) {
-            instance = new GameEngine(); 
-            currentPlayer = playerX; 
-            gameBoard = new char[3][3]; 
+        currentPlayer = playerX; 
+        gameBoard = new String[3][3]; 
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                gameBoard[i][j] = ""; 
+            }
         }
     }
 
     /**
-     * Checks if the game is over
-     * @return true if the game is over, false if it is not
+     * Checks if there is a winner. 
+     * @return true if any row, column or diagonal contains 3 of the same players tokens, false if not
      */
-    public boolean isGameOver() {
-        if ((gameBoard[0][0] == gameBoard[1][1] && gameBoard[0][0] == gameBoard[2][2]) || 
-            (gameBoard[0][2] == gameBoard[1][1] && gameBoard[0][2] == gameBoard[2][0]) || 
-            (gameBoard[1][0] == gameBoard[1][1] && gameBoard[1][0] == gameBoard[1][2]) || 
-            (gameBoard[0][1] == gameBoard[1][1] && gameBoard[0][1] == gameBoard[2][1]) ||
-            (gameBoard[0][0] == gameBoard[0][1] && gameBoard[0][0] == gameBoard[0][2]) || 
-            (gameBoard[0][0] == gameBoard[1][0] && gameBoard[0][0] == gameBoard[2][0]) ||
-            (gameBoard[2][0] == gameBoard[2][1] && gameBoard[2][0] == gameBoard[2][2]) || 
-            (gameBoard[2][0] == gameBoard[1][0] && gameBoard[2][0] == gameBoard[0][0])) {
+    public boolean isWinner() {
+        if (gameBoard[0][0] == gameBoard[1][1] && gameBoard[0][0] == gameBoard[2][2] && !gameBoard[1][1].equals("") || // diagonal TL-BR
+            gameBoard[0][2] == gameBoard[1][1] && gameBoard[0][2] == gameBoard[2][0] && !gameBoard[1][1].equals("") || // diagonal TR-BL
+            gameBoard[1][0] == gameBoard[1][1] && gameBoard[1][0] == gameBoard[1][2] && !gameBoard[1][1].equals("") || // middle row
+            gameBoard[0][1] == gameBoard[1][1] && gameBoard[0][1] == gameBoard[2][1] && !gameBoard[1][1].equals("") || // middle column
+            gameBoard[0][0] == gameBoard[0][1] && gameBoard[0][0] == gameBoard[0][2] && !gameBoard[0][0].equals("") || // top row
+            gameBoard[0][0] == gameBoard[1][0] && gameBoard[0][0] == gameBoard[2][0] && !gameBoard[0][0].equals("") || // left column
+            gameBoard[2][0] == gameBoard[2][1] && gameBoard[2][0] == gameBoard[2][2] && !gameBoard[2][2].equals("") || // bottom row
+            gameBoard[0][2] == gameBoard[1][2] && gameBoard[0][2] == gameBoard[2][2] && !gameBoard[2][2].equals("")) { // right column
             return true;  
         }
         return false; 
     }
 
     /**
-     * Gets the currentPlayer
-     * @return currentPlayer
+     * Gets the currentPlayer.
+     * @return currentPlayer.
      */
-    public char getCurrentPlayer() {
+    public String getCurrentPlayer() {
         return currentPlayer; 
     }
 
     /**
      * Gets the game board for the GUI
+     * ## Currently the GUI sets the buttons and then sets the back end game board
+     * ## It may be better if this were reversed. (This is not currently used.)
      * @return 3x3 String array representing the game board
      */
-    public char[][] getGameBoard() {
+    public String[][] getGameBoard() {
         return gameBoard; 
     }
 
     /**
-     * Place a token on the board
+     * Tracks where tokens are placed on the board. 
+     * @param row the row to place the token.
+     * @param col teh column to place the token. 
+     * @param player the X or O value to be placed on the board. 
+     * @throws IndexOutOfBoundsException if row or col is any value other than 0, 1, 2
      */
-    public void placeToken(int x, int y) {
-        if (' ' != gameBoard[x][y]) throw new IllegalArgumentException("This space is already taken."); 
-        gameBoard[x][y] = currentPlayer; 
+    public void setToken(int row, int col, String player) {
+        if (0 > row || 0 > col || row > 2 || col > 2) throw new IndexOutOfBoundsException("This is not a valid board location"); 
+        gameBoard[row][col] = player; 
     }
 
     /**
-     * Switch player
+     * Switch currentPlayer. 
      */
     public void switchPlayer() {
         if (currentPlayer == playerX) {
--- README.md
+++ README.md
@@ -4,3 +4,11 @@ Java based Tic Tac Toe Game
 # 4/28/2021
 - Building this game for fun - primarily to experiment with designing a GUI. 
 - Working on the game logic
+
+# 4/29/2021
+- GUI launches - used line boarders to create the tic tac toe board grid
+- Bug with game - after playing the first X and O it sticks on O 
+- - - Bug fixed! 
+- Updated Javadoc throughout
+- Added pop up error message when trying to play on a spot that's already been claimed
+- Fully working game - had the kids test it!! 
